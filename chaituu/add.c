e8795bbddfe259011e2c2a9445e1e4e7e586a4a6
bossmool <bossmool@boss>
2016-01-26 17:39:04 +0530

    https://github.com/chaitu1704/test1.git

---
+++ chaituu/add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include</home/bossmool/chaituu/sum.h>
+extern int sum();
+void main()
+{
+int k,g;
+printf("enter the 1st number:");
+scanf("%d",&k);
+printf("enter the 2nd number:");
+scanf("%d",&g);
+sum(k,g);
+}
---
+++ chaituu/sum.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+#include</home/bossmool/chaituu/sum.h>
+int sum(int x,int y)
+{
+printf("%5d+%5d=%5d",x,y,(x+y));
+return 0;
+}
